276d27ec5c25a0296bfffc3dc286b727b57551ed
sugatakenji <sugatakenji@gmail.com>
2022-09-27 20:37:41 -0300

    1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import * as THREE from '/three';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
